9bd1042e4e513aba6b97a72635d7ba9671c2917e
Anderson Rodrigues <eusouanderson@outlook.com>
2025-01-30 11:16:35 -0300

    teste

--- modulo-01/helloworld/app.ts
+++ modulo-01/helloworld/app.ts
@@ -1,10 +1,6 @@
-/**
- * arquivo: helloworld/app.ts
- * descrição: arquivo responsável por ensinar conceitos básicos sobre TypeScript
- * data: 16/02/2021
- * author: Glaucia Lemos <@glaucia_lemos86>
- * Transpilação do arquivo: <Windows> CTRL + SHIFT + B
- */
+// Hello World !
 
-let mensagem: string = 'Hello World, Glaucia Lemos!';
-console.log(mensagem);
+let nome: string = 'Anderson';
+let mensagem: string = `Ola, ${nome}!`;
+
+console.log(mensagem)
